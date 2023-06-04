d24027bff0b1af65d524c5765b881379a763e04c
yurtercan <ercanyurt@yahoo.com>
2023-06-05 01:17:11 +0300

    -

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const {randomBytes} =require('crypto');
+const cors = require('cors');
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -12,7 +15,7 @@ app.get('/posts/:id/comments',(req,res)=>{
 
 app.post('/posts/:id/comments',(req,res)=>{
     const commentId = randomBytes(4).toString('hex');
-    const content = req.body;
+    const {content} = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({id:commentId,content});
